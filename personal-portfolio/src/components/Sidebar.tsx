c00fef5698f14fa38679023800584866be8cc68d
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2025-12-31 05:18:54 +0100

    configuring the side bar

--- personal-portfolio/src/components/Sidebar.tsx
+++ personal-portfolio/src/components/Sidebar.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
+import  {Sheet, SheetContent, SheetTitle, 
+    SheetTrigger} from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
+import { MenuIcon } from 'lucide-react';
+import { navLinks, socialLinks } from '@/constants';
+
 
 export const Sidebar = () => {
+    const [active, setActive] = useState('#Hero');
   return (
-    <div>
-      Sidebar
-    </div>
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+            <Button variant='ghost' 
+            size='icon'
+            className='m-4 fixed top-4 right-4 z-50
+            border-2 hover:border-primary bg-neutral-800 
+            py-5 px-5 hover:text-primary cursor-pointer'
+        > 
+                <MenuIcon size={30}/> 
+            </Button>
+        </SheetTrigger>
+        <SheetContent 
+            side='right' 
+            className='w-84  bg-neutral-900  text-white
+            py-6 pl-10'
+            bg-neutral-900
+        >
+
+        </SheetContent>
+      </Sheet>
+    </>
   )
 }
 
