726b5e959983a70b39611e9027d77472b3d8e5ca
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2026-01-04 23:35:36 +0100

    update

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -15,6 +15,7 @@ export const App = () => {
       <Hero/>
       <Stats />
       <Projects />
+      Ab
     </main>
     
   )
--- personal-portfolio/src/components/Projects.tsx
+++ personal-portfolio/src/components/Projects.tsx
@@ -5,6 +5,7 @@ import {  staggerContainer } from '@/lib/animation';
 import {projectsData} from '@/constants';
 import { Section } from 'lucide-react';
 import { SectionHeader } from '@/components/SectionHeader';
+import { ProjectCard } from '@/components/ProjectCard';
 
 
 export const Projects = () => {
@@ -21,6 +22,23 @@ export const Projects = () => {
             subtitle="Projects" 
             title="My featured projects"
         />
+
+        <motion.div
+            className='grid md:grid-cols-2 gap-10 mt-10'
+            initial="hidden"
+            whileInView='visible'
+            viewport={{ once: true, amount: 0.3}}
+            variants={staggerContainer(0.5)}
+        >
+            {projectsData.map((project, index) => (
+                <ProjectCard 
+                    key={index} imgSrc={project.imgSrc}
+                    projectLink={project.projectLink}
+                    tags={project.tags}
+                    title={project.title}
+                />
+            ))}
+        </motion.div>
    </motion.section>
   )
 }
